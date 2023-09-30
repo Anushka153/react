d7ee8197568c25d8ed271491e4d4afc705620f67
Anushka <anuvns153@gmail.com>
2023-09-30 11:03:32 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement('h1', { id: "heading", axy: 'gag' }, 'Hello World React');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const parent = React.createElement('div', { id: 'parent' }, [React.createElement('div', { id: 'child' }, [heading, heading]), React.createElement('div', { id: 'child' }, [heading, heading])])
+
+root.render(parent);
+console.log('heading', parent, root)
+
