5ec0e2a8e8681afd4a4bd5a284fa9cabdf432576
Anushka <anuvns153@gmail.com>
2023-10-06 12:27:25 +0530

    react add

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement('h1', { id: "heading", axy: 'gag' }, 'Hello World React');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
